901747089506a2108f5636ce991fba5b97e2f299
Supragya Raj <supragyaraj@gmail.com>
2018-04-06 21:15:53 +0530

    render success

--- templating_system.js
+++ templating_system.js
@@ -1,22 +1,24 @@
+
 var handlebars = require('handlebars'),
-  fs = require('fs');
+fs = require('fs');
 
 var data = JSON.parse(fs.readFileSync('newsletter_data/20180406_000/data.json', 'utf8'));
 data.body = process.argv[2];
 
 fs.readFile('template.html', 'utf-8', function(error, source){
-  handlebars.registerHelper('custom_title', function(title){
+    handlebars.registerHelper('custom_title', function(title){
     var words = title.split(' ');
     for (var i = 0; i < words.length; i++) {
-      if (words[i].length > 4) {
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-      }
+        if (words[i].length > 4) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        }
     }
     title = words.join(' ');
     return title;
-  })
+})
 
-  var template = handlebars.compile(source);
-  var html = template(data);
-  console.log(html)
+    var template = handlebars.compile(source);
+    var html = template(data);
+    fs.writeFile("render.html", html);
+    console.log(html)
 });
